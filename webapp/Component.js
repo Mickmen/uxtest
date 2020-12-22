d1312e1608d9bc443240a19dada8400ddfe21276
Mickmen <m.englbrecht@web.de>
2020-12-22 14:07:24 +0000

    update

--- webapp/Component.js
+++ webapp/Component.js
@@ -10,9 +10,10 @@ sap.ui.define([
 		metadata: {
 			manifest: "json"
 		},   
-
+   
 		/**
 		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+         * Test
 		 * @public
 		 * @override
 		 */
